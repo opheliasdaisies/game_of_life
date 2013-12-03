945e16a06c2cda24cba08ae762c7416ba96f4e25
Sara Gorecki <sgorecki@gmail.com>
2013-12-03 14:59:15 -0500

    revise tests for board to include all_cells variable

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,4 +1,5 @@
 require_relative "../lib/board"
+require_relative "../lib/cell"
 
 RSpec.configure do |config|
   # Use color in STDOUT
@@ -15,14 +16,14 @@ describe "Board" do
 
 	it "Creates a board with a dead cell in each position" do
 		board = Board.new(3,4)
-		game = board.create
-		game[0][0].state.should eq("dead")
+		board.create
+		board.all_cells[0][0].state.should eq("dead")
 	end
 
 	it "Creates a board with a dead cell in each position" do
 		board = Board.new(3,4)
-		game = board.create
-		game[3][4].state.should eq("dead")
+		board.create
+		board.all_cells[3][4].state.should eq("dead")
 	end
 
 end
