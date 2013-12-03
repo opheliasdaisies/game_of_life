b584534caea637f7e814fd32503c2cc385a6e42c
Sara Gorecki <sgorecki@gmail.com>
2013-12-03 14:53:44 -0500

    build initialize mehtod for Board, and begin create method

---
+++ lib/board.rb
@@ -0,0 +1,15 @@
+class Board
+	attr_reader :height, :width
+	attr_accessor :all_cells
+
+	def initialize(height, width)
+		@height = height
+		@width = width
+		@all_cells = []
+	end
+
+	def create
+				
+	end
+
+end
