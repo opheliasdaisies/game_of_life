a811fac339c9f5662936a074dc97acd71a5f1d47
Sara Gorecki <sgorecki@gmail.com>
2013-12-03 18:52:19 -0500

    add code to let a cell find its position on the board

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,8 +1,28 @@
 class Cell
-	attr_accessor :state
+	attr_accessor :state, :neighbors
+	attr_reader :position
 
 	def initialize(state="dead")
 		@state = state
+		@neighbors = []
+	end
+
+	def find_neighbors(board)
+		
+	end
+
+	def find_position(board)
+		coords = []
+		board.all_cells.each do |row|
+			row.each do |cell|
+				if cell == self
+					coords << board.all_cells.index(row)
+					coords << row.index(cell)
+				end
+			end
+		end
+		@position = coords
 	end
 
 end
+
